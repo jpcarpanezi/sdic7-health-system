5dd9bd5549803e1ea58dcc73857cc8cef77fcd27
Murilo Azevedo Jacon <murilo.ajac@gmail.com>
2022-04-24 22:52:29 -0300

    Ocelot API Gateway Setup

--- src/ApiGateway/Gateway.Ocelot/Program.cs
+++ src/ApiGateway/Gateway.Ocelot/Program.cs
@@ -1,5 +1,28 @@
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options => {
+	var origins = builder.Configuration.GetSection("CorsOrigin").Get<string[]>();
+	options.AddDefaultPolicy(build => {
+		build.SetIsOriginAllowedToAllowWildcardSubdomains()
+			.WithOrigins(origins)
+			.AllowAnyMethod()
+			.AllowAnyHeader()
+			.AllowCredentials();
+	});
+});
+
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("ocelot.json").Build();
+builder.Services.AddOcelot(configuration).AddCacheManager(settings => settings.WithDictionaryHandle());
+
 var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseCors();
+await app.UseOcelot();
 
 app.MapGet("/", () => "Hello World!");
 
