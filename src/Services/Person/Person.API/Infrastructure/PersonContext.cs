378e1f67ee0b3c758db6f4a0b39b6db7f739bedc
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2022-06-06 18:19:29 -0300

    Fixed wrong references and removed warning

--- src/Services/Person/Person.API/Infrastructure/PersonContext.cs
+++ src/Services/Person/Person.API/Infrastructure/PersonContext.cs
@@ -18,13 +18,6 @@ namespace Person.API.Infrastructure {
 		public virtual DbSet<MedicalInformation> MedicalInformations { get; set; }
 		public virtual DbSet<Models.Entities.Person> People { get; set; }
 
-		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-			if (!optionsBuilder.IsConfigured) {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-				optionsBuilder.UseMySql("server=localhost;port=3306;database=HealthPerson;uid=root;pwd=1234;guidformat=Binary16", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.25-mysql"));
-			}
-		}
-
 		protected override void OnModelCreating(ModelBuilder modelBuilder) {
 			modelBuilder.UseCollation("utf8mb4_0900_ai_ci")
 				.HasCharSet("utf8mb4");
@@ -45,7 +38,7 @@ namespace Person.API.Infrastructure {
 
 				entity.HasOne(d => d.Person)
 					.WithMany(p => p.EmergencyContacts)
-					.HasForeignKey(d => d.PUuid)
+					.HasForeignKey(d => d.PersonUUID)
 					.HasConstraintName("fk_EmergencyContacts_Person");
 			});
 
