a3ee72557361ae615bc3e8bd0c33ad04c12a7119
IsabelleCCC <isa.ccc@live.com>
2022-06-26 18:17:56 -0300

    Medicine and Prescription Controller, Model, View

---
+++ src/Services/Medicines/Medicines.API/Models/Updates/UpdateMedicine.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Medicines.API.Models.Updates {
+	public class UpdateMedicine {
+		[Required]
+		public Guid UUID { get; set; }
+
+		[Required]
+		[MaxLength(255)]
+		public string DrugName { get; set; }
+
+		[Required(AllowEmptyStrings = false)]
+		[MaxLength(255)]
+		public string ActiveIngredient { get; set; }
+
+		[Required(AllowEmptyStrings = false)]
+		[MaxLength(255)]
+		public string FormRoute { get; set; }
+
+		[Required(AllowEmptyStrings = false)]
+		[MaxLength(255)]
+		public string Company { get; set; }
+	}
+}
---
+++ src/Services/Medicines/Medicines.API/Models/Updates/UpdatePrescription.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Medicines.API.Models.Updates {
+	public class UpdatePrescription {
+		[Required]
+		public Guid UUID { get; set; }
+
+		public Guid MedicalConsultationUUID { get; set; }
+
+		public Guid MedicineUUID { get; set; }
+
+		[Required(AllowEmptyStrings = false)]
+		[MaxLength(64)]
+		public string Dosage { get; set; }
+	}
+}
